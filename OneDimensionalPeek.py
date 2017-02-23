f0d68f85bdf5cc9b931cb1b6fa9b555213c638d2
Liwangchun <Liwangchun@meituan.com>
2017-02-23 16:18:30 +0800

    添加二分

--- OneDimensionalPeek.py
+++ OneDimensionalPeek.py
@@ -5,10 +5,10 @@
 # You may imagine that num[-1] = num[n] = -∞.
 # For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
 class Peek(object):
-	def findPeakElement(self,nums):
+	def findPeakElement1(self,nums):
 		numsLen = len(nums)
 		if numsLen <= 1 :
-			return 1
+			return 0
 		for index,num in enumerate(nums):
 			peek=num
 			if index>0:
@@ -21,10 +21,35 @@ class Peek(object):
 			elif peek > nums[index+1]:
 				return index
 
+	def findPeakElement2(self,nums):
+		numsLen = len(nums)
+		if numsLen <= 1 :
+			return 0
+		return self.find(0,numsLen,nums)
 
-
+	def find(self,start,end,nums):
+		if start==end:
+			return start
+		index = (int)((start+end)/2)
+		midNum = nums[index]
+		if index > start:
+			if index < end-1 :
+				if midNum > nums[index-1] and midNum > nums[index+1]:
+					return index
+				elif midNum > nums[index-1]:
+					return self.find(index,end,nums)
+				else:
+					return self.find(0,index+1,nums)
+			elif  midNum>nums[index-1]:
+					return index
+			else:
+				return index-1;
+		elif midNum>nums[index+1]:
+			return index
+		else:
+			return index+1;
 
 if __name__ == '__main__':
 	peek = Peek();
-	index=peek.findPeakElement([1,2])
+	index=peek.findPeakElement2(range(1000000))
 	print(index)
